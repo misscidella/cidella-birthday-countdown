918edca51a59b6563f43f5c205616c920d569c8d
misscidella <nyamandevr4@gmail.com>
2024-06-04 22:11:49 +0200

    last commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const CicisBirtday = new Date('June 08, 2024 00:00:00')
-const countdownDisplay = document.getElementById('countdown');
+let countdownDisplay = document.getElementById('countdown');
 const daysDisplay = document.getElementById('days');
 const hoursDisplay = document.getElementById('hours');
 const minutesDisplay = document.getElementById('minutes');
@@ -8,20 +8,33 @@ const secondsDisplay = document.getElementById('seconds');
 const updateCountdown=()=>{
 	const diff = CicisBirtday -new Date();
 	const timeleft=getTimeLeft(diff);
-	daysDisplay.innerText=`${timeleft.days}  `;
-	hoursDisplay.innerText=`${timeleft.hours} `;
-	minutesDisplay.innerText=`${timeleft.minutes} `;
-	secondsDisplay.innerText=`${timeleft.seconds} `;
+	daysDisplay.innerText=`${timeleft.days}`;
+	hoursDisplay.innerText=`${timeleft.hours}`;
+	minutesDisplay.innerText=`${timeleft.minutes}`;
+	secondsDisplay.innerText=`${timeleft.seconds}`;
+	if (timeleft.days < 0 && 
+		timeleft.hours  < 0 && 
+		timeleft.minutes < 0 &&
+		timeleft.seconds < 0){
+		countdownDisplay.innerText=("HAPPY BIRTHDAY  CiCi");
+	countdownDisplay.classList.add('birthday-message')
+	document.body.style.pointerEvents="none";
+	const screenDisplay = document.querySelectorAll('h1,h2');
+	screenDisplay.forEach(element=>{
+		element.classList.add('hidden')
+		});
+	}
 }
 
 const getTimeLeft = (diff) => {
   return {
-    days: Math.floor(diff / (1000 * 60 * 60 * 24)), // days in days
-    hours: Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)), // hours in hours
-    minutes: Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60)), // minutes in minutes
-    seconds: Math.floor((diff % (1000 * 60)) / 1000) // seconds in seconds
+    days: Math.floor(diff / (1000 * 60 * 60 * 24)), 
+    hours: Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)), 
+    minutes: Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60)), 
+    seconds: Math.floor((diff % (1000 * 60)) / 1000) 
   };
 };
 
-countdownDisplay.addEventListener("input" ,updateCountdown());
 setInterval(updateCountdown, 1000);
+countdownDisplay(updateCountdown());
+
